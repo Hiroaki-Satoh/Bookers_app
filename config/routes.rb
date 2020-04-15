07e573e5b74a70e716abfde3aff4c0da9f3da5ce
Hiroaki-Satoh <yurayuraclover.liebe@gmail.com>
2020-04-15 20:05:20 +0900

    [Finish] 4-8

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # indexからdestroyまでの7つのルーティングを定義する。
+  resources :books
+
+  get 'top' => 'books#top'
+  #ルートへアクセスした際のルーティング設定
+  root to: 'books#top'
+
 end
